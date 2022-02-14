3a5e03b027cc78c652d5e93af64866516c46397b
Teemu Hakala <thakala@c1r6p16.hive.fi>
2022-02-14 12:14:13 +0200

    eval_tests right from the start, included allowed libraries

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,11 +6,13 @@
 /*   By: thakala <thakala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 05:48:32 by thakala           #+#    #+#             */
-/*   Updated: 2022/02/10 05:49:01 by thakala          ###   ########.fr       */
+/*   Updated: 2022/02/14 11:57:10 by thakala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
 #endif
